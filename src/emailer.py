655809b209dd71b163f8f6c1ace37a7d2a76d308
ethanwilli84 <117854791+ethanwilli84@users.noreply.github.com>
2026-03-29 23:10:02 -0400

    Update emailer.py

--- src/emailer.py
+++ src/emailer.py
@@ -9,7 +9,7 @@ TEMPLATE = """Hey, I wanted to reach out to see what the process looks like for 
 
 I haven't done too many public appearances in the past since I live a pretty private life, but I'm looking to start doing more because I genuinely believe my story can inspire others and my message moves people. I've spoken at a few schools and to entrepreneur groups but I'd really like to make a larger impact on a broader scale.
 
-For context, I'm 20 years old, based in New York City, and I founded a software company that now does a little over $5 million per year in revenue. I also lead a community of young entrepreneurs called the Taco Project, pretty interesting origin story, but all good people actually making a difference in the world.
+For context, I'm 20 years old, based in New York City, and I founded a software company, Sire, that now does a little over $5 million per year in revenue. I also lead a community of young entrepreneurs called the Taco Project, pretty interesting origin story, but all good people actually making a difference in the world.
 
 Would love to learn more about the process and what the upcoming calendar looks like for you guys.
 
